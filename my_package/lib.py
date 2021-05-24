27b361bee3a06e0c9d5fc1d347a617fc74a496e5
Naomi Caduff <naomi.caduff@gmail.com>
2021-05-24 14:31:42 +0200

    updated lib file and added colleagues' functions

--- my_package/lib.py
+++ my_package/lib.py
@@ -2,3 +2,5 @@ def try_me():
     your_name = input("What's your name? ") 
     print(f"Hi {your_name}, welcome to Le Wagon Data Science bootcamp batch 606")
     
+if __name__ == "__main__":
+    try_me()
